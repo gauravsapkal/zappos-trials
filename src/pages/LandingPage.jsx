c697b99729c945b8c10c245f8d63b775d6c788fb
Gaurav Sapkal <gauravsapkal1997@gmail.com>
2022-10-10 17:23:18 +0530

    project

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import { Box, Heading } from "@chakra-ui/react"
+
+
+const LandingPage = ()=>{
+
+
+    return(
+        <Box>
+            <Heading>Hello Zappos</Heading>
+        </Box>
+    )
+}
+
+export default LandingPage;
