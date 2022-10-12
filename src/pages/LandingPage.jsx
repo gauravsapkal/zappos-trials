d9cd9d21ac09313c63dd7389404a630a4cea0356
Gaurav Sapkal <gauravsapkal1997@gmail.com>
2022-10-12 21:56:18 +0530

    big banner2

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import { Box } from "@chakra-ui/react"
 import BelowNavbarBanner from "../components/landing-page/BelowNavbarBanner";
+import Bigbanner2 from "../components/landing-page/Bigbanner2";
 import Bigbanners from "../components/landing-page/Bigbanners";
 import PopularCategory from "../components/landing-page/PopularCategory";
 import PopularItems from "../components/landing-page/PopularItems";
@@ -14,6 +15,7 @@ const LandingPage = ()=>{
             <PopularCategory/>
             <Bigbanners/>
             <PopularItems/>
+            <Bigbanner2/>
         </Box>
     )
 }
